dbf841b7c3b9be5e972c511127283bcd718f1cb7
euforia <euforia@gmail.com>
2015-09-29 09:25:22 -0700

    Fixed level display width

--- simplelog.go
+++ simplelog.go
@@ -28,11 +28,11 @@ func NewLogger(
 	warningHandle io.Writer,
 	errorHandle io.Writer) *Logger {
 
-	trace := log.New(traceHandle, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
-	debug := log.New(traceHandle, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-	info := log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	warning := log.New(warningHandle, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	err := log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	trace := log.New(traceHandle, "TRC: ", log.Ldate|log.Ltime|log.Lshortfile)
+	debug := log.New(traceHandle, "DBG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	info := log.New(infoHandle, "INF: ", log.Ldate|log.Ltime|log.Lshortfile)
+	warning := log.New(warningHandle, "WRN: ", log.Ldate|log.Ltime|log.Lshortfile)
+	err := log.New(errorHandle, "ERR: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	return &Logger{trace, debug, info, warning, err}
 }
@@ -46,22 +46,22 @@ func (l *Logger) SetLogLevel(level string) error {
 	case "trace":
 		break
 	case "debug":
-		l.Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Trace = log.New(ioutil.Discard, "TRC: ", log.Ldate|log.Ltime|log.Lshortfile)
 		break
 	case "info":
-		l.Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Debug = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Trace = log.New(ioutil.Discard, "TRC: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Debug = log.New(ioutil.Discard, "DBG: ", log.Ldate|log.Ltime|log.Lshortfile)
 		break
 	case "warning":
-		l.Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Debug = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Info = log.New(ioutil.Discard, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Trace = log.New(ioutil.Discard, "TRC: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Debug = log.New(ioutil.Discard, "DBG: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Info = log.New(ioutil.Discard, "INF: ", log.Ldate|log.Ltime|log.Lshortfile)
 		break
 	case "error":
-		l.Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Debug = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Info = log.New(ioutil.Discard, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		l.Warning = log.New(ioutil.Discard, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Trace = log.New(ioutil.Discard, "TRC: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Debug = log.New(ioutil.Discard, "DBG: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Info = log.New(ioutil.Discard, "INF: ", log.Ldate|log.Ltime|log.Lshortfile)
+		l.Warning = log.New(ioutil.Discard, "WRN: ", log.Ldate|log.Ltime|log.Lshortfile)
 		break
 	default:
 		return errors.New(fmt.Sprintf("Invalid log level: %s", level))
